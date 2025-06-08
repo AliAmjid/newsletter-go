863e2da00d872b9910b6552f6c17e86adc8afb15
Ali <ali.amjid@mystay.cz>
2025-06-08 18:08:18 +0200

    fix: swagger

--- domain/post.go
+++ domain/post.go
@@ -1,14 +1,6 @@
 package domain
 
-import "context"
-
-// Post represents a newsletter post.
 type Post struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
-}
-
-// PostRepository defines the persistence functions for posts.
-type PostRepository interface {
-	Store(ctx context.Context, p *Post) error
 }
