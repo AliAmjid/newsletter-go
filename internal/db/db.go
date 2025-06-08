e22093b2f647ecfb8a6883657eb76ce1906b8be0
Ali Amjid <32981703+AliAmjid@users.noreply.github.com>
2025-06-08 17:42:19 +0200

    Add DI container and retrying DB init

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -11,14 +12,20 @@ var DB *sql.DB
 
 func Init(dataSourceName string) {
 	var err error
-	DB, err = sql.Open("postgres", dataSourceName)
-
-	if err != nil {
-		log.Fatal("Failed to open database:", err)
-	}
-
-	if err := DB.Ping(); err != nil {
-		log.Fatal("Failed to connect to database:", err)
+	for {
+		DB, err = sql.Open("postgres", dataSourceName)
+		if err != nil {
+			log.Printf("Failed to open database: %v. Retrying in 5 seconds...", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		if err = DB.Ping(); err != nil {
+			log.Printf("Failed to connect to database: %v. Retrying in 5 seconds...", err)
+			DB.Close()
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		break
 	}
 
 	log.Println("Connected to DB")
