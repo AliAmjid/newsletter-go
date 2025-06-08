6a3a1b5099467d38f3b406d28bede580cb9126f9
Ali <ali.amjid@mystay.cz>
2025-06-08 18:15:38 +0200

    post fied:

--- domain/post.go
+++ domain/post.go
@@ -1,6 +1,12 @@
 package domain
 
+import "context"
+
 type Post struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
+}
+
+type PostRepository interface {
+	Store(ctx context.Context, p *Post) error
 }
