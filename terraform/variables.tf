68cfe41ffdb6b788808c5f7aca25870b253ce075
KubaDavid <jakub.david@resonect.cz>
2025-06-14 20:15:39 +0200

    ci: add ec2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-central-1"
+  default     = "eu-central-1"
 }
 
 variable "aws_account_id" {
@@ -12,4 +12,21 @@ variable "aws_account_id" {
 variable "aws_profile" {
   description = "AWS CLI profile to use (from your ~/.aws/credentials)"
   default     = "development"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0a87a69d69fa289be"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ec2_key_name" {
+  description = "SSH key pair name for EC2 instance"
+  type        = string
 }
